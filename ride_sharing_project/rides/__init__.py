88ef2c0f7421edfb34d06f8a58452be9e40e9f29
ar791 <ankit.raj@duke.edu>
2025-02-03 12:55:32 -0500

    fixed admin db view, added admin.py

--- ride_sharing_project/rides/__init__.py
+++ ride_sharing_project/rides/__init__.py
@@ -1,8 +1,1 @@
 default_app_config = 'rides.apps.RidesConfig'
-from django.contrib import admin
-from .models import User, Vehicle, Ride, RideSharer
-
-admin.site.register(User)
-admin.site.register(Vehicle)
-admin.site.register(Ride)
-admin.site.register(RideSharer)
