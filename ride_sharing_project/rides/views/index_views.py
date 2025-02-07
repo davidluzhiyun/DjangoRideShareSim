2256cf3f707ced300143f3d5ef5e44185a9f36bf
davidluzhiyun <zl303@duke.edu>
2025-02-07 17:42:15 -0500

    fix: Added My Profile to dashboard, fixed redirection behavior

--- ride_sharing_project/rides/views/index_views.py
+++ ride_sharing_project/rides/views/index_views.py
@@ -3,6 +3,6 @@ from django.urls import reverse
 
 def index_redirect(request):
     if request.user.is_authenticated:
-        return redirect(reverse('rides:list'))
+        return redirect(reverse('rides:my_rides'))
     else:
         return redirect(reverse('accounts:login'))
